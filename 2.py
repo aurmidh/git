a8f51ddac26920648ad1718f7a476c8c09fd8e32
Lena Danilova <68924623+aurmidh@users.noreply.github.com>
2023-12-02 11:30:45 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,24 @@
+class Figure(ABC):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    @abstractmethod
+    def calculateArea():
+        pass
+
+
+class Triangle(Figure):
+    def calculateArea(self):
+        return 0, 5 * self.width
+
+
+class Rectangle(Figure):
+    def calculateArea(self):
+        return self.height * self.width * self.height
+
+
+figures = [Rectangle(10, 5), Triangle(10, 5)]
+
+for figure in figures:
+    print(figure.calculateArea())
